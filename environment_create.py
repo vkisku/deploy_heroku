51f13165b3baae322c4e2bd060a3daec24e524fc
Vikash Kisku <vikashkisku@gmail.com>
2020-01-01 17:36:21 +0530

    Added tkinter for environement create. The list of Environemnt is done. Work with the environment  list is to be done

--- environment_create.py
+++ environment_create.py
@@ -5,26 +5,31 @@ import time
 import configparser
 import os
 import django_app_deploy as deploy
+import sys
+import subprocess
 
 class App:
 	def __init__(self, window, window_title, video_source=0):
 		self.config = configparser.ConfigParser()
 		self.scale = 100
 		self.window = window
-		self.window.geometry("500x130+300+300")
+		self.window.geometry("500x180+300+300")
 		self.window.title(window_title)
 		self.directoryFlag=0
-
-		self.label = Label(self.window,text="Follow the steps to deploy Django app to heroku").place(x=10,y=10)
-		self.label1 = Label(self.window,text="1. Browse and select The Folder to your Project folder where settings.py is present").place(x=10,y=30)
-		self.label2 = Label(self.window,text="2.Press Deploy").place(x=10,y=50)
-		self.folderbutton = Button(self.window,text="Browse Folder",width=12,height=1,command=self.get_dirname).place(x=10,y=70)
+		self.createdflag=0
+		self.label = Label(self.window,text="Prerequisite: Pip should be installed.").place(x=10,y=10)
+		self.label = Label(self.window,text="Follow the steps to create Virtual Environment").place(x=10,y=30)
+		self.label1 = Label(self.window,text="1. Enter the Name of the Unique Virtual Environment").place(x=10,y=50)
+		self.label2 = Label(self.window,text="2.Press Create").place(x=10,y=70)
+		self.label2 = Label(self.window,text="Environment Name:").place(x=10,y=90)
+		self.environment=StringVar()
+		self.EnvironmentEntry = Entry(self.window,textvariable = self.environment,width=20).place(x=155,y=90)
 		self.folderSuccess = StringVar()
 		self.folderError = StringVar()
 
-		self.folderLabelSuccess = Label(self.window, fg='green', textvariable=self.folderSuccess).place(x=150,y=70)
-		self.folderLabelError 	= Label(self.window, fg='red', textvariable=self.folderError).place(x=150,y=70)
-		self.deploybutton = Button(self.window,text="Deploy",width=12,height=1,command=self.deploy).place(x=190,y=100)
+		self.folderLabelSuccess = Label(self.window, fg='green', textvariable=self.folderSuccess).place(x=150,y=90)
+		self.folderLabelError 	= Label(self.window, fg='red', textvariable=self.folderError).place(x=150,y=90)
+		self.deploybutton = Button(self.window,text="Create",width=12,height=1,command=self.list_virtual_env).place(x=190,y=130)
 
 		self.menubar = Menu(self.window)
 		self.filemenu = Menu(self.menubar,tearoff = 0)
@@ -47,17 +52,38 @@ class App:
 		self.barcode_current_data=""
 		self.barcode_previous_data=""
 		self.window.mainloop()
+
+	def list_virtual_env(self):
+		vlist=subprocess.check_output("lsvirtualenv",shell=True)
+		vlist=vlist.decode()
+		vlist=vlist.split('\r\n')
+		vlist=[x for x in vlist if x]
+		#vlist=set(vlist)
+		print(vlist)
+		if self.environment.get() in vlist:
+			messagebox.showinfo('Alert','Environment ALready Exits.')
+
 	
-	def deploy(self):
-		if self.directoryFlag == 0:
-			messagebox.showinfo("Alert", "Please Select Project Folder")
-		elif self.directoryFlag == -1:
-			messagebox.showinfo("ALert","Please Select Correct Project Folder")
+	def create(self):
+		if(not(self.environment.get() and (self.environment.get()).strip())):
+			messagebox.showinfo("Alert", "Enter The environment name!")
 		else:
-			deploy.Django_Heroku_Deploy(self.folderSuccess.get())
-			messagebox.showinfo("Success","Success")
-			print('Successfully Configured for Deployment.')
-			self.window.quit()
+			self.createdflag=1
+			print(self.environment.get())
+			try:
+				subprocess.call("pip install virtualenvwrapper",shell=True)
+				subprocess.call("pip install virtualenv",shell=True)
+				try:
+					subprocess.call("mkvirtualenv "+self.environment.get(),shell=True)
+					print("Environment",self.environment.get(),"created Successfully.")
+					messagebox.showinfo("Success","Environment "+self.environment.get()+" created Successfully.")
+				except Exception as e:
+					print(e)
+					
+			except Exception as e:
+				print(e)
+
+			#self.window.quit()
 
 	def do_nothing(self):
 		print('do Nothing')
